d1a004775a83f2f4f72a65304914d65086c944f7
ashish24 <ashishbhagat2425@gmail.com>
2023-05-03 23:29:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './sass/App.scss';
+
+import  TodoWrapper  from './components/TodoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper />
+    </div>
+  );
+}
+
+export default App;
